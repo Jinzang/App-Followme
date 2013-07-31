168998745fdfa1be6e345a3637ff041ed99f1963
Bernie Simon <bernie.simon@gmail.com>
2013-07-31 19:11:49 -0400

    New package to initialize new web site

---
+++ lib/App/FollowmeSite.pm
@@ -0,0 +1,159 @@
+package App::FollowmeSite;
+use 5.008005;
+use strict;
+use warnings;
+
+use IO::File;
+
+our $VERSION = "0.71";
+our $modeline;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(copy_site);
+
+#----------------------------------------------------------------------
+# Create a copy of the input file
+
+sub copy_file {
+    my ($file, $text, $directory) = @_;
+
+    my @dirs;    
+    push(@dirs, split(/\//, $directory)) if $directory;
+    push(@dirs, split(/\//, $file));
+    my $base = pop(@dirs);
+    
+    my $path = '.';
+    foreach my $dir (@dirs) {
+        $path .= "/$dir";
+
+        if (! -d $path) {
+            mkdir ($path) or die "Couldn't create $path: $!\n";
+        }
+    }    
+    $path .= "/$base";
+    
+    my $out = IO::File->new($path, 'w') or die "Can't write $path";
+
+    chomp $text;
+    print $out $text;
+        
+    close($out);
+    return;
+}
+
+#----------------------------------------------------------------------
+# Copy website
+
+sub copy_site {
+    my ($directory) = @_;
+    
+    while (my ($file, $text) = next_file()) {
+        eval {copy_file($file, $text, $directory)};
+        warn "$file: $@" if $@;
+    }
+
+    return;
+}
+
+#----------------------------------------------------------------------
+# Get the name and contents of the next file
+
+sub next_file {
+    
+    $modeline ||= <DATA>;
+    return unless $modeline;
+    
+    my ($comment, $file) = split(' ', $modeline);
+    die "Bad modeline: $modeline\n" unless defined $file;
+    
+    my $text = '';
+    $modeline = '';
+    
+    while (<DATA>) {
+        if (/^\#--\%X--\%X/) {
+            $modeline = $_;
+            last;
+
+        } else {
+            $text .= $_;
+        }
+    }
+    
+    return ($file, $text);
+}
+
+1;
+__DATA__
+#--%X--%X template.html
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- begin meta -->
+<title>{{title}}</title>
+<!-- end meta -->
+</head>
+<body>
+<div id="header">
+<h1>Site Title</h1>
+</div>
+<div id="content">
+<!-- begin content -->
+<h2>{{title}}</h2>
+
+{{body}}    
+<!-- end content-->
+</div>
+</body>
+</html>
+
+#--%X--%X blog_template.html
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- begin meta -->
+<title>{{title}}</title>
+<!-- end meta -->
+</head>
+<body>
+<div id="header">
+<h1>Site Title</h1>
+</div>
+<div id="content">
+<!-- begin content -->
+<!-- loop -->
+{{body}}
+<p><a href="{{url}}">Written on {{month}} {{day}}, {{year}}</a></p>
+<!-- endloop -->
+<!-- end content-->
+</div>
+</body>
+</html>
+
+#--%X--%X archive/index_template.html
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- begin meta -->
+<title>{{title}}</title>
+<!-- end meta -->
+</head>
+<body>
+<div id="header">
+<h1>Site Title</h1>
+</div>
+<div id="content">
+<!-- begin content -->
+<h2>{{title}}</h2>
+
+<ul>
+<!-- loop --><li><a href="{{url}}">{{title}}</a></li>
+<!-- endloop -->
+</ul>
+<!-- end content-->
+</div>
+</body>
+</html>
+
---
+++ t/FollowmeSite.t
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+use strict;
+
+use IO::File;
+use Test::More tests => 3;
+
+#----------------------------------------------------------------------
+# Load package
+
+my @path = split(/\//, $0);
+pop(@path);
+
+my $bin = join('/', @path);
+my $lib = "$bin/../lib";
+unshift(@INC, $lib);
+
+require App::FollowmeSite;
+
+my $test_dir = "$bin/../test";
+system("/bin/rm -rf $test_dir");
+mkdir $test_dir;
+chdir $test_dir;
+
+#----------------------------------------------------------------------
+# Test next_file
+
+do {
+    my (@files, @texts);
+    while(my ($file, $text) = App::FollowmeSite::next_file()) {
+        push(@files, $file);
+        push(@texts, $text);
+    }
+    
+    my @files_ok = qw(template.html blog_template.html
+                      archive/index_template.html);
+
+    is_deeply(\@files, \@files_ok, "Next file name"); # test 1
+    
+    my @long = grep {length($_) > 100} @texts;
+    is(@long, 3, "Next file"); # test 2
+    
+    my $file = shift @files;
+    my $text = shift @texts;
+    App::FollowmeSite::copy_file($file, $text);
+    ok(-e $file, 'Copy file'); #test 3
+};
