6cb046942d67397122097fc8357a28159e1e8853
Bernie Simon <bernie.simon@gmail.com>
2013-09-17 19:28:34 -0400

    Bumped version number to 0.89

--- lib/App/Followme.pm
+++ lib/App/Followme.pm
@@ -12,7 +12,7 @@ use Digest::MD5 qw(md5_hex);
 use File::Spec::Functions qw(abs2rel splitdir catfile);
 use App::FollowmeSite qw(copy_file next_file);
 
-our $VERSION = "0.88";
+our $VERSION = "0.89";
 
 require Exporter;
 our @ISA = qw(Exporter);
--- lib/App/FollowmeSite.pm
+++ lib/App/FollowmeSite.pm
@@ -6,7 +6,7 @@ use warnings;
 use IO::File;
 use File::Spec::Functions qw(splitdir catfile);
 
-our $VERSION = "0.88";
+our $VERSION = "0.89";
 our $modeline;
 
 require Exporter;
