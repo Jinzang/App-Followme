d471923bb67446d86170604e0d59d97c59f3ef4e
Bernie Simon <bernie.simon@gmail.com>
2013-11-11 18:28:15 -0500

    Initial version of class

---
+++ lib/App/Followme/MostRecentFile.pm
@@ -0,0 +1,57 @@
+package App::Followme::MostRecentFile;
+use 5.008005;
+use strict;
+use warnings;
+
+use lib '../..';
+
+our $VERSION = "0.93";
+
+use File::Spec::Functions qw(rel2abs);
+use base qw(App::Followme::EveryFile);
+
+#----------------------------------------------------------------------
+# Read the default parameter values
+
+sub parameters {
+    my ($pkg) = @_;
+    
+    my %parameters = (
+                count => 0,
+                web_extension => 'html',
+           );
+
+    my %base_params = $pkg->SUPER::parameters();
+    %parameters = (%base_params, %parameters);
+
+    return %parameters;
+}
+
+#---------------------------------------------------------------------
+# Return the next file
+
+sub next {
+    my ($self) = @_;
+    
+    return if $self->{count} ++;
+    return $self->visit;
+}
+
+#----------------------------------------------------------------------
+# Return 1 if filename passes test
+
+sub match_file {
+    my ($self, $path) = @_;
+    return ! -d $path && $path =~ /\.$self->{web_extension}$/;
+}
+
+#----------------------------------------------------------------------
+# Return 1 if folder passes test
+
+sub match_folder {
+    my ($self, $path) = @_;
+    return;
+}
+
+1;
+__END__
