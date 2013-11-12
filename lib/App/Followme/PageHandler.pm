13dad1e6161fdae1a96ceaceb664095c87e6cf03
Bernie Simon <bernie.simon@gmail.com>
2013-11-11 19:58:05 -0500

    Initial version for base class for converters

---
+++ lib/App/Followme/PageHandler.pm
@@ -0,0 +1,486 @@
+package App::Followme::PageHandler;
+use 5.008005;
+use strict;
+use warnings;
+
+use lib '../..';
+
+use Cwd;
+use IO::File;
+use File::Spec::Functions qw(abs2rel catfile rel2abs splitdir updir);
+
+use App::Followme::TopDirectory;
+use base qw(App::Followme::EveryFile);
+
+our $VERSION = "0.93";
+
+use constant MONTHS => [qw(January February March April May June July
+                           August September October November December)];
+
+#----------------------------------------------------------------------
+# Read the default parameter values
+
+sub parameters {
+    my ($pkg) = @_;
+    
+    my %parameters = (
+            absolute => 0,
+            quick_update => 0,
+            web_extension => 'html',
+           );
+
+    my %base_params = $pkg->SUPER::parameters();
+    %parameters = (%base_params, %parameters);
+
+    return %parameters;
+}
+
+#----------------------------------------------------------------------
+# Convert text files into web pages
+
+sub run {
+    my ($self) = @_;
+
+    my $template = $self->make_template();
+    my $sub = $self->compile_template($template);
+
+    while (defined(my $filename = $self->next)) {
+        eval {$self->handle_page($filename, $sub)};
+        warn "$filename: $@" if $@;
+    }
+
+    return ! $self->{quick_update};    
+}
+
+#----------------------------------------------------------------------
+# Build date fields from time, based on Blosxom 3
+
+sub build_date {
+    my ($self, $data, $filename) = @_;
+    
+    my $num = '01';
+    my $months = MONTHS;
+    my %month2num = map {substr($_, 0, 3) => $num ++} @$months;
+
+    my $time;
+    if (-e $filename) {
+        my @stats = stat($filename);
+        $time = $stats[9];
+    } else {
+        $time = time();
+    }
+    
+    my $ctime = localtime($time);
+    my @names = qw(weekday month day hour24 minute second year);
+    my @values = split(/\W+/, $ctime);
+
+    while (@names) {
+        my $name = shift @names;
+        my $value = shift @values;
+        $data->{$name} = $value;
+    }
+
+    $data->{day} = sprintf("%02d", $data->{day});
+    $data->{monthnum} = $month2num{$data->{month}};
+
+    my $hr = $data->{hour24};
+    if ($hr < 12) {
+        $data->{ampm} = 'am';
+    } else {
+        $data->{ampm} = 'pm';
+        $hr -= 12;
+    }
+
+    $hr = 12 if $hr == 0;
+    $data->{hour} = sprintf("%02d", $hr);
+
+    return $data;
+}
+
+#----------------------------------------------------------------------
+# Get the title from the filename root
+
+sub build_title {
+    my ($self, $data, $filename) = @_;
+    
+    my ($dir, $root, $ext) = $self->parse_filename($filename);
+
+    if ($root eq 'index') {
+        my @dirs = splitdir($dir);
+        $root = pop(@dirs) || '';
+    }
+    
+    $root =~ s/^\d+// unless $root =~ /^\d+$/;
+    my @words = map {ucfirst $_} split(/\-/, $root);
+    $data->{title} = join(' ', @words);
+    
+    return $data;
+}
+
+#----------------------------------------------------------------------
+# Build a url from a filename
+
+sub build_url {
+    my ($self, $data, $filename) = @_;
+
+    my $directory = $self->{absolute}
+                  ? App::Followme::TopDirectory->name()
+                  : $self->{base_directory};
+                  
+    $filename = catfile($filename, 'index.html') if -d $filename;
+    $filename = rel2abs($filename);
+    $filename = abs2rel($filename, $directory);
+
+    my $url = join('/', splitdir($filename));
+    $url = "/$url" if $self->{absolute};
+    $url =~ s/\.[^\.]*$/.$self->{web_extension}/;
+
+    $data->{url} = $url;
+    return $data;
+}
+
+#----------------------------------------------------------------------
+# Compile a template into a subroutine
+
+sub compile_template {
+    my ($self, $template, $variable) = @_;
+    $variable = '{{*}}' unless $variable;
+    
+    my ($left, $right) = split(/\*/, $variable);
+    $left = quotemeta($left);
+    $right = quotemeta($right);
+    
+    my $code = <<'EOQ';
+sub {
+my ($data) = @_;
+my ($block, @blocks);
+EOQ
+
+    my @tokens = split(/(<!--\s*(?:loop|endloop).*?-->)/, $template);
+    
+    foreach my $token (@tokens) {
+        if ($token =~ /^<!--\s*loop/) {
+            $code .= 'foreach my $data (@{$data->{loop}}) {' . "\n";
+
+        } elsif ($token =~ /^<!--\s*endloop/) {
+            $code .= "}\n";
+
+        } else {
+            $code .= "\$block = <<'EOQ';\n";
+            $code .= "${token}\nEOQ\n";
+            $code .= "chomp \$block;\n";
+            $code .= "\$block =~ s/$left(\\w+)$right/\$data->{\$1}/g;\n";
+            $code .= "push(\@blocks,\$block);\n";
+        }
+    }
+    
+    $code .= <<'EOQ';
+return join('', @blocks);
+}
+EOQ
+
+    my $sub = eval ($code);
+    die $@ unless $sub;
+    return $sub;
+}
+
+#----------------------------------------------------------------------
+# Get fields external to file content
+
+sub external_fields {
+    my ($self, $data, $filename) = @_;
+
+    $data = $self->build_date($data, $filename);
+    $data = $self->build_title($data, $filename);
+    $data = $self->build_url($data, $filename);
+
+    return $data;
+}
+
+#----------------------------------------------------------------------
+# Find an file to serve as a prototype for updating other files
+
+sub find_prototype {
+    my ($self, $uplevel) = @_;
+
+    my $filename;
+    my $directory = $self->{base_directory};
+    my $top_directory = App::Followme::TopDirectory->name;
+    
+    for (;;) {
+        if ($uplevel) {
+            $uplevel -= 1;
+
+        } else {
+            my $mrf = MostRecentFile->new($directory);
+            $filename = $mrf->next;
+            last if $filename;
+        }
+
+        last if $directory eq $top_directory;
+        chdir(updir());
+        $directory = getcwd();
+    }
+
+    chdir($self->{base_directory});
+    return $filename;
+}
+
+#----------------------------------------------------------------------
+# Get the full template name (stub)
+
+sub get_template_name {
+    my ($self) = @_;
+    return catfile($self->{base_directory}, 'template.htm');
+}
+
+#----------------------------------------------------------------------
+# Handle the changes on a single page
+
+sub handle_page {
+    my ($self, $filename, $sub) = @_;
+    
+    my $data = {};
+    $data = $self->external_fields($data, $filename);
+    $data = $self->internal_fields($data, $filename);
+    my $page = $sub->($data);
+
+    $self->write_page($filename, $page);
+    unlink($filename);
+
+    return;
+}
+
+#----------------------------------------------------------------------
+# Get fields from reading the file (stub)
+
+sub internal_fields {
+    my ($self, $data, $filename) = @_;   
+
+    $data->{body} = $self->read_page($filename);
+    return $data;
+}
+
+#----------------------------------------------------------------------
+# Combine template with prototype
+
+sub make_template {
+    my ($self) = @_;
+
+    my $template_name = $self->get_template_name();
+    my $template = $self->read_page($template_name);
+    die "Couldn't find template: $template_name\n" unless $template;
+
+    my $prototype_name = $self->find_prototype(0);
+    my $prototype = $self->read_page($prototype_name); 
+    
+    my $final_template;
+    if ($prototype) {
+        my $decorated = 0;
+        my $prototype_path = {};
+        $final_template = $self->update_page($prototype, $template, 
+                                             $decorated, $prototype_path);
+    } else {
+        $final_template = $template;
+    }
+
+    return $final_template;
+}
+
+#----------------------------------------------------------------------
+# Return 1 if filename passes test
+
+sub match_file {
+    my ($self, $path) = @_;
+    return ! -d $path && $path =~ /\.$self->{web_extension}$/;
+}
+
+#----------------------------------------------------------------------
+# Return 1 if folder passes test
+
+sub match_folder {
+    my ($self, $path) = @_;
+    return;
+}
+
+#----------------------------------------------------------------------
+# Parse fields out of section tag
+
+sub parse_blockname {
+    my ($self, $str) = @_;
+    
+    my ($blockname, $in, $locality) = split(/\s+/, $str);
+    
+    if ($in) {
+        die "Syntax error in block ($str)"
+            unless $in eq 'in' && defined $locality;
+    } else {
+        $locality = '';
+    }
+    
+    return ($blockname, $locality);
+}
+
+#----------------------------------------------------------------------
+# Break page into blocks
+
+sub parse_blocks {
+    my ($self, $page, $decorated, $block_handler, $prototype_handler) = @_;
+    
+    my $locality;
+    my $block = '';
+    my $blockname = '';
+    my @tokens = split(/(<!--\s*(?:section|endsection)\s+.*?-->)/, $page);
+    
+    foreach my $token (@tokens) {
+        if ($token =~ /^<!--\s*section\s+(.*?)-->/) {
+            die "Improperly nested block ($token)\n" if $blockname;
+                
+            ($blockname, $locality) = $self->parse_blockname($1);
+            if ($decorated) {
+                $block .= $token
+            } else {
+                $prototype_handler->($token);
+            }
+            
+        } elsif ($token =~ /^<!--\s*endsection\s+(.*?)-->/) {
+            my ($endname) = $self->parse_blockname($1);
+            die "Unmatched ($token)\n"
+                if $blockname eq '' || $blockname ne $endname;
+                
+            if ($decorated) {
+                $block .= $token;
+                $block_handler->($blockname, $locality, $block);
+            } else {
+                $block_handler->($blockname, $locality, $block);
+                $prototype_handler->($token);
+            }
+
+            $block = '';
+            $blockname = '';
+
+        } else {
+            if ($blockname) {
+                $block .= $token;
+            } else {
+                $prototype_handler->($token);
+            }            
+        }
+    }
+ 
+    die "Unmatched block (<!-- section $blockname -->)\n" if $blockname;
+    return;
+}
+
+#----------------------------------------------------------------------
+# Parse filename into directory, root, and extension
+
+sub parse_filename {
+    my ($self, $filename) = @_;
+
+    my @dirs = splitdir($filename);
+    my $basename = pop(@dirs);
+    my ($root, $ext) = split(/\./, $basename);
+    my $dir = @dirs ? catfile(@dirs) : '';
+    
+    return ($dir, $root, $ext);
+}
+
+#----------------------------------------------------------------------
+# Extract named blocks from a page
+
+sub parse_page {
+    my ($self, $page, $decorated) = @_;
+    
+    my $blocks = {};
+    my $block_handler = sub {
+        my ($blockname, $locality, $blocktext) = @_;
+        if (exists $blocks->{$blockname}) {
+            die "Duplicate block name ($blockname)\n";
+        }
+        $blocks->{$blockname} = $blocktext;
+        return;
+    };
+    
+    my $prototype_handler = sub {
+        return;
+    };
+
+    $self->parse_blocks($page, $decorated, $block_handler, $prototype_handler);    
+    return $blocks;
+}
+
+#----------------------------------------------------------------------
+# Read a file into a string
+
+sub read_page {
+    my ($self, $filename) = @_;
+
+    local $/;
+    my $fd = IO::File->new($filename, 'r');
+    return unless $fd;
+    
+    my $page = <$fd>;
+    close($fd);
+    
+    return $page;
+}
+
+#----------------------------------------------------------------------
+# Parse prototype and page and combine them
+
+sub update_page {
+    my ($self, $prototype, $page, $decorated, $prototype_path) = @_;
+
+    my $output = [];
+    my $blocks = $self->parse_page($page, $decorated);
+    
+    my $block_handler = sub {
+        my ($blockname, $locality, $blocktext) = @_;
+        if (exists $blocks->{$blockname}) {
+            if (exists $prototype_path->{$locality}) {
+                push(@$output, $blocktext);          
+            } else {
+                push(@$output, $blocks->{$blockname});
+            }
+            delete $blocks->{$blockname};
+        } else {
+            push(@$output, $blocktext);
+        }
+        return;
+    };
+
+    my $prototype_handler = sub {
+        my ($blocktext) = @_;
+        push(@$output, $blocktext);
+        return;
+    };
+
+    $self->parse_blocks($prototype, $decorated,
+                        $block_handler, $prototype_handler);
+
+    if (%$blocks) {
+        my $names = join(' ', sort keys %$blocks);
+        die "Unused blocks ($names)\n";
+    }
+    
+    return join('', @$output);
+}
+
+#----------------------------------------------------------------------
+# Write the page back to the file
+
+sub write_page {
+    my ($self, $filename, $page) = @_;
+
+    my $fd = IO::File->new($filename, 'w');
+    die "Couldn't write $filename" unless $fd;
+    
+    print $fd $page;
+    close($fd);
+        
+    return;
+}
+
+1;
+__END__
---
+++ t/PageHandler.t
@@ -0,0 +1,371 @@
+#!/usr/bin/env perl
+use strict;
+
+use IO::File;
+use File::Path qw(rmtree);
+use File::Spec::Functions qw(catdir catfile rel2abs splitdir);
+
+use Test::More tests => 30;
+
+#----------------------------------------------------------------------
+# Load package
+
+my @path = splitdir(rel2abs($0));
+pop(@path);
+pop(@path);
+
+my $lib = catdir(@path, 'lib');
+unshift(@INC, $lib);
+
+require App::Followme::PageHandler;
+require App::Followme::TopDirectory;
+
+my $test_dir = catdir(@path, 'test');
+
+rmtree($test_dir);
+mkdir $test_dir;
+mkdir "$test_dir/sub";
+chdir $test_dir;
+
+App::Followme::TopDirectory->name($test_dir);
+
+#----------------------------------------------------------------------
+# Test parse_blocks
+
+do {
+    my $blocks = {};
+    my $block_handler = sub {
+        my ($blockname, $in, $blocktext) = @_;
+        $blocks->{$blockname} = $blocktext;
+        return;
+    };
+    
+    my $prototype = [];
+    my $template_handler = sub {
+        my ($blocktext) = @_;
+        push(@$prototype, $blocktext);
+        return;
+    };
+    
+    my @page = (
+                "Top line",
+                "<!-- section first in folder -->",
+                "First block",
+                "<!-- endsection first -->",
+                "Middle line",
+                "<!-- section second -->",
+                "Second block",
+                "<!-- endsection second -->",
+                "Last line",
+               );
+
+    my $page = join("\n", @page) . "\n";
+    
+    my $decorated = 0;
+    my $ph = App::Followme::PageHandler->new;
+    $ph->parse_blocks($page, $decorated, $block_handler, $template_handler);
+
+    my $ok_blocks = {
+        first => "\nFirst block\n",
+        second => "\nSecond block\n",
+    };
+
+    my $ok_prototype = [
+                        "Top line\n",
+                        "<!-- section first in folder -->",
+                        "<!-- endsection first -->",
+                        "\nMiddle line\n",
+                        "<!-- section second -->",
+                        "<!-- endsection second -->",
+                        "\nLast line\n",
+                           ];
+
+    is_deeply($blocks, $ok_blocks, 'Parse blocks'); # test 1
+    is_deeply($prototype, $ok_prototype, 'Parse prototype'); # test 2
+    
+    $blocks = {};
+    $prototype = [];
+    my @bad_page = @page;
+    pop(@bad_page); pop(@bad_page);
+    $page =join("\n", @bad_page) . "\n";
+
+    eval {
+        $ph->parse_blocks($page, $decorated,
+                                            $block_handler,
+                                            $template_handler);
+    };
+    is($@, "Unmatched block (<!-- section second -->)\n", 'Missing end'); # test 3
+
+    @bad_page = @page;
+    shift(@bad_page); shift(@bad_page);
+    $page =join("\n", @bad_page) . "\n";
+
+    eval {
+        $ph->parse_blocks($page, $decorated,
+                                            $block_handler,
+                                            $template_handler);
+    };
+    is($@, "Unmatched (<!-- endsection first -->)\n", 'Missing begin'); # test 4
+
+    @bad_page = @page;
+    splice(@bad_page, 3, 1);
+    $page =join("\n", @bad_page) . "\n";
+
+    eval {
+        $ph->parse_blocks($page, $decorated,
+                                            $block_handler,
+                                            $template_handler);
+    };
+    is($@, "Improperly nested block (<!-- section second -->)\n",
+       'Begin inside of begin'); # test 5
+
+    @bad_page = @page;
+    splice(@bad_page, 3, 3);
+    $page =join("\n", @bad_page) . "\n";
+
+    eval {
+        $ph->parse_blocks($page, $decorated,
+                                            $block_handler,
+                                            $template_handler);
+    };
+    is($@, "Unmatched (<!-- endsection second -->)\n",
+       'Begin does not match end'); # test 6
+};
+
+#----------------------------------------------------------------------
+# Test parse_page
+
+do {
+    my @page = (
+                "Top line",
+                "<!-- section first -->",
+                "First block",
+                "<!-- endsection first -->",
+                "Middle line",
+                "<!-- section second -->",
+                "Second block",
+                "<!-- endsection second -->",
+                "Last line",
+               );
+
+    my $decorated = 0;
+    my $page = join("\n", @page) . "\n";
+    my $ph = App::Followme::PageHandler->new;
+    my $blocks = $ph->parse_page($page, $decorated);
+
+    my $ok_blocks = {
+        first => "\nFirst block\n",
+        second => "\nSecond block\n",
+    };
+    
+    is_deeply($blocks, $ok_blocks, 'Parse undecorated blocks'); # test 7
+
+    $decorated = 1;
+    $blocks = $ph->parse_page($page, $decorated);
+
+    $ok_blocks = {
+        first => "<!-- section first -->\nFirst block\n<!-- endsection first -->",
+        second => "<!-- section second -->\nSecond block\n<!-- endsection second -->",
+    };
+
+    is_deeply($blocks, $ok_blocks, 'Parse decorated blocks'); # test 8
+
+    my $bad_page = $page;
+    $bad_page =~ s/second/first/g;
+    $blocks = eval {$ph->parse_page($bad_page, $decorated)};
+    
+    is($@, "Duplicate block name (first)\n", 'Duplicate block names'); # test 9
+};
+
+#----------------------------------------------------------------------
+# Test update_page
+
+do {
+    my @prototype = (
+                "Top line",
+                "<!-- section first in folder -->",
+                "First block",
+                "<!-- endsection first -->",
+                "Middle line",
+                "<!-- section second -->",
+                "Second block",
+                "<!-- endsection second -->",
+                "Last line",
+               );
+
+    my $prototype = join("\n", @prototype) . "\n";
+
+    my $decorated = 0;
+    my $page = $prototype;
+    $page =~ s/line/portion/g;
+    $page =~ s/block/section/g;
+    
+    my $prototype_path = {folder => 1};
+    my $ph = App::Followme::PageHandler->new;
+    my $output = $ph->update_page($prototype, $page,
+                                                    $decorated,
+                                                    $prototype_path);
+    my @output = split(/\n/, $output);
+    
+    my @output_ok = @prototype;
+    $output_ok[6] =~ s/block/section/;
+
+    is_deeply(\@output, \@output_ok, 'Update page'); # test 10
+
+    my $bad_page = $page;
+    $bad_page =~ s/second/third/g;
+
+    $output = eval{$ph->update_page($prototype, $bad_page,
+                                                      $decorated, $prototype_path)};
+    is($@, "Unused blocks (third)\n", 'Update page bad block'); # test 11
+};
+
+#----------------------------------------------------------------------
+# Test read and write page
+
+do {
+   my $code = <<'EOQ';
+<html>
+<head>
+<meta name="robots" content="archive">
+<!-- section meta -->
+<title>%%</title>
+<!-- endsection meta -->
+</head>
+<body>
+<!-- section content -->
+<h1>%%</h1>
+<!-- endsection content -->
+<!-- section navigation in folder -->
+<p><a href="">&&</a></p>
+<!-- endsection navigation -->
+</body>
+</html>
+EOQ
+
+    my $ph = App::Followme::PageHandler->new;
+    foreach my $dir (('sub', '')) {
+        foreach my $count (qw(four three two one)) {
+            sleep(1);
+            my $output = $code;
+            $output =~ s/%%/Page $count/g;
+            $output =~ s/&&/$dir link/g;
+
+            my @dirs;
+            push(@dirs, $test_dir);
+            push(@dirs, $dir) if $dir;
+
+            my $filename = catfile(@dirs, "$count.html");
+            $ph->write_page($filename, $output);
+
+            my $input = $ph->read_page($filename);
+            is($input, $output, "Read and write page $filename"); #tests 12-20
+        }
+    }
+};
+
+#----------------------------------------------------------------------
+# Test converters
+
+do {
+   my $text = <<'EOQ';
+<p>This is a paragraph</p>
+
+<pre>
+This is preformatted text.
+</pre>
+EOQ
+
+   my $template = <<'EOQ';
+<html>
+<head>
+<meta name="robots" content="archive">
+<!-- section meta -->
+<title>{{title}}</title>
+<!-- endsection meta -->
+</head>
+<body>
+<!-- section content -->
+<h1>{{title}}</h1>
+
+{{body}}
+
+<ul>
+<!-- loop -->
+<li>{{count}} {{item}}</li>
+<!-- endloop -->
+</ul>
+<!-- endsection content -->
+</body>
+</html>
+EOQ
+
+    my @loop;
+    my $i = 0;
+    foreach my $word (qw(one two three four)) {
+        $i = $i + 1;
+        push(@loop, {count => $i, item => $word});
+    }
+    
+    my $data = {title =>'Three', body => $text, loop => \@loop};
+
+    my $ph = App::Followme::PageHandler->new;
+    my $sub = $ph->compile_template($template);
+    my $page = $sub->($data);
+
+    ok($page =~ /<h1>Three<\/h1>/, 'Apply template to title'); # test 20
+    ok($page =~ /<p>This is a paragraph<\/p>/,
+       'Apply template to body'); # test 21
+
+    my @li = $page =~ /(<li>)/g;
+    is(@li, 4, 'Loop over data items'); # test 22
+    ok($page =~ /<li>2 two<\/li>/, 'Substitute in loop'); # test 23
+};
+
+#----------------------------------------------------------------------
+# Test builders
+
+do {
+    my $dir = App::Followme::TopDirectory->name;
+    chdir($dir);
+    
+    my $data = {};
+    my $ph = App::Followme::PageHandler->new;
+    my $text_name = catfile('watch','this-is-only-a-test.txt');
+    
+    $data = $ph->build_title($data, $text_name);
+    my $title_ok = 'This Is Only A Test';
+    is($data->{title}, $title_ok, 'Build file title'); # test 24
+
+    my $index_name = catfile('watch','index.html');
+    $data = $ph->build_title($data, $index_name);
+    $title_ok = 'Watch';
+    is($data->{title}, $title_ok, 'Build directory title'); # test 25
+    
+    $data = $ph->build_url($data, $text_name);
+    my $url_ok = 'watch/this-is-only-a-test.html';
+    is($data->{url}, $url_ok, 'Build rleativr file url'); # test 26
+
+    $ph->{absolute} = 1;
+    $data = $ph->build_url($data, $text_name);
+    $url_ok = '/watch/this-is-only-a-test.html';
+    is($data->{url}, $url_ok, 'Build rleativr file url'); # test 27
+
+    mkdir('watch');
+    $data = $ph->build_url($data, 'watch');
+    is($data->{url}, '/watch/index.html', 'Build directory url'); #test 28
+       
+    $data = {};
+    my $date = $ph->build_date($data, 'two.html');
+    my @date_fields = grep {/\S/} sort keys %$date;
+    my @date_ok = sort qw(day month monthnum  weekday hour24 hour 
+                   minute second year ampm);
+    is_deeply(\@date_fields, \@date_ok, 'Build date'); # test 29
+    
+    $data = {};
+    $data = $ph->external_fields($data, 'two.html');
+    my @keys = sort keys %$data;
+    my @keys_ok = sort(@date_ok, 'title', 'url');
+    is_deeply(\@keys, \@keys_ok, 'Get data for file'); # test 30
+};
+
