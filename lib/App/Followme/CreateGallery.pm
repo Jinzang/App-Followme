0aaf25a9b0547ea8dec5255c9ec30aa46883fe5a
Jinzang <bernie.simon@gmail.com>
2015-05-16 16:03:27 -0400

    Added module to create photo gallery

---
+++ lib/App/Followme/CreateGallery.pm
@@ -0,0 +1,406 @@
+package App::Followme::CreateGallery;
+use 5.008005;
+use strict;
+use warnings;
+
+use lib '../..';
+
+use base qw(App::Followme::Module);
+
+use GD;
+use Cwd;
+use IO::Dir;
+use File::Spec::Functions qw(abs2rel rel2abs splitdir catfile);
+
+our $VERSION = "1.12";
+
+#----------------------------------------------------------------------
+# Read the default parameter values
+
+sub parameters {
+    my ($self) = @_;
+
+    return (
+                      gallery_file => 'index.html',
+                      gallery_include => '*.jpg',
+                      gallery_template => 'gallery.htm',
+                      thumb_suffix => '-thumb',
+                      thumb_width => 0,
+                      thumb_height => 0,
+                      photo_width => 0,
+                      photo_height => 0,
+                     );
+}
+
+#----------------------------------------------------------------------
+# Return all the files in a subtree (example)
+
+sub run {
+    my ($self, $directory) = @_;
+
+    my $gallery_name = $self->full_file_name($directory, $self->{gallery_file});
+
+    eval {
+       $self->create_a_gallery($directory, $gallery_name)
+    };
+
+    warn "$gallery_name: $@" if $@;
+
+    return;
+}
+
+#----------------------------------------------------------------------
+# Build the name of the photo file
+
+sub build_photo_name {
+    my ($self, $filename) = @_;
+    return $filename;
+}
+
+#----------------------------------------------------------------------
+# Build a url from a photo name
+
+sub build_photo_url {
+    my ($self, $data, $directory, $filename) = @_;
+
+    for my $field (qw(thumb photo)) {
+        my $name = $field . '_url';
+        my $sub = "build_${field}_name";
+
+        my $photoname = $self->$sub($filename, $field);
+        $data->{$name} = $self->filename_to_url($directory,
+                                                $photoname,
+                                               );
+
+        $name = 'absolute_' . $name;
+        $data->{$name} = '/' . $self->filename_to_url($self->{top_directory},
+                                                      $photoname,
+                                                     );
+    }
+
+    return $data;
+}
+
+#----------------------------------------------------------------------
+# Build the name of the thumb file
+
+sub build_thumb_name {
+    my ($self, $filename) = @_;
+
+    my ($dir, $file) = $self->split_filename($filename);
+    my ($root, $ext) = split(/\./, $file);
+    $file = join('', $root, $self->{thumb_suffix}, '.', $ext);
+    my $photoname = catfile($dir, $file);
+
+    return $photoname;
+}
+
+#----------------------------------------------------------------------
+# Create the photo gallery for a directory
+
+sub create_a_gallery {
+    my ($self, $directory, $gallery_name) = @_;
+
+    my ($filenames, $directories) = $self->visit($directory);
+    @$filenames = grep {$self->match_file($_)} @$filenames;
+
+    my $template_name = $self->get_template_name($self->{gallery_template});
+    return if $self->is_newer($gallery_name, $template_name, @$filenames);
+
+    my $data = $self->SUPER::set_fields($directory, $gallery_name);
+    $data->{loop} = $self->gallery_data($directory, $filenames);
+
+    my $render = $self->make_template($gallery_name,
+                                      $self->{gallery_template});
+    my $page = $render->($data);
+
+    $self->write_page($gallery_name, $page);
+    return;
+}
+
+#----------------------------------------------------------------------
+# Get data to be interpolated into template
+
+sub gallery_data {
+    my ($self, $directory, $filenames) = @_;
+
+    my @index_data;
+    foreach my $filename (@$filenames) {
+        push(@index_data, $self->set_fields($directory, $filename));
+    }
+
+    return \@index_data;
+}
+
+#----------------------------------------------------------------------
+# Get the dimensions of an image and its thumbnail, resize if needed
+
+sub get_dimensions {
+    my ($self, $data, $directory, $filename) = @_;
+
+    my $gallery_name = $self->full_file_name($directory, $self->{gallery_file});
+
+    my $old_photo = $self->read_photo($filename);
+    my ($old_width, $old_height) = $old_photo->getBounds();
+
+    for my $field (qw(thumb photo)) {
+        my ($width, $height) = $self->new_size($field, $old_width, $old_height);
+        $data->{"${field}_width"} = $width;
+        $data->{"${field}_height"} = $height;
+
+        my $sub = "build_${field}_name";
+        my $photoname = $self->$sub($filename);
+        next unless $self->is_newer($photoname, $gallery_name);
+
+        my $photo = $self->resize_a_photo($old_photo, $width, $height,
+                                          $old_width, $old_height);
+
+        $self->write_photo($photoname, $photo);
+    }
+
+    return $data;
+}
+
+#----------------------------------------------------------------------
+# Get the list of excluded files
+
+sub get_excluded_files {
+    my ($self) = @_;
+
+    my ($dir, $file) = $self->split_filename($self->{gallery_include});
+    $file = $self->build_thumb_name($file);
+    ($dir, $file) = $self->split_filename($file);
+
+    return $file;
+}
+
+#----------------------------------------------------------------------
+# Get the list of included files
+
+sub get_included_files {
+    my ($self) = @_;
+    return $self->{gallery_include};
+}
+
+#---------------------------------------------------------------------------
+# Calculate the new width and height of a photo
+
+sub new_size {
+    my($self, $field, $old_width, $old_height) = @_;
+
+    my $width_field = "${field}_width";
+    my $height_field = "${field}_height";
+
+    my $factor;
+    if ($self->{$width_field} && $self->{$height_field}) {
+        my $width_factor = $self->{$width_field} / $old_width;
+        my $height_factor = $self->{$height_field} / $old_height;
+        $factor = ($height_factor < $width_factor ? $height_factor : $width_factor);
+
+    } elsif ($self->{$width_field}) {
+       $factor = $self->{$width_field} / $old_width;
+
+    } elsif ($self->{$height_field}) {
+        $factor = $self->{$height_field} / $old_height;
+
+    } else {
+        die "$width_field and $height_field not set";
+    }
+
+    my $height = int($factor * $old_height);
+    my $width  = int($factor * $old_width);
+
+    return ($width, $height);
+}
+
+#---------------------------------------------------------------------------
+# Read a photo
+
+sub read_photo {
+    my ($self, $photoname) = @_;
+
+    GD::Image->trueColor(1);
+    my $photo = GD::Image->new($photoname);
+    die "Couldn't read $photoname" unless $photo;
+
+    return $photo;
+}
+
+#---------------------------------------------------------------------------
+# Resize a photo
+
+sub resize_a_photo {
+    my ($self, $old_photo, $width, $height, $old_width, $old_height) = @_;
+
+    my $photo = GD::Image->new($width, $height);
+    $photo->copyResampled($old_photo,
+                          0, 0, 0, 0,
+                          $width, $height,
+                          $old_width, $old_height);
+
+    return $photo;
+}
+
+#----------------------------------------------------------------------
+# Set the data fields for a file
+
+sub set_fields {
+    my ($self, $directory, $filename) = @_;
+
+    my $data = {};
+    $data = $self->build_date($data, $filename);
+    $data = $self->build_title_from_filename($data, $filename);
+    $data = $self->build_photo_url($data, $directory, $filename);
+    $data = $self->get_dimensions($data, $directory, $filename);
+
+    return $data;
+}
+
+#----------------------------------------------------------------------
+# Save a photo
+
+sub write_photo {
+    my ($self, $photoname, $photo) = @_;
+
+    my $fd = IO::File->new($photoname, 'w');
+    die "Couldn't write $photoname" unless $fd;
+
+    my $data = $photo->jpeg();
+
+    binmode($fd);
+    print $fd $data;
+    close($fd);
+
+    return;
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Followme::CreateGallery - Create a photo gallery page
+
+=head1 SYNOPSIS
+
+    use App::Followme::CreateGallery;
+    my $gallery = App::Followme::CreateGallery->new($configuration);
+    $gallery->run($directory);
+
+=head1 DESCRIPTION
+
+This package builds an index for a directory which serves as a photo gallery.
+The variables described below aresubstituted into a template to produce the
+gallery. Loop comments that look like
+
+    <!-- loop -->
+    <!-- endloop -->
+
+indicate the section of the template that is repeated for each photo contained
+in the directory. The following variables may be used in the template:
+
+=over 4
+
+=item absolute_photo_url
+
+The absolute url of the photo
+
+=item absolute_thumb_url
+
+The absolute url of the photo thumbnail
+
+=item photo_height
+
+The height of the photo
+
+=item photo_url
+
+The relative url of the photo
+
+=item photo_width
+
+The width of the photo
+
+=item thumb_height
+
+The height of the photo thumbnail
+
+=item thumb_url
+
+The relative url of the photo thumbnail
+
+=item thumb_width
+
+The width of the photo thumbnail
+
+=item title
+
+The photo tile, derived from the photo filename.
+
+=item time fields
+
+The variables calculated from the modification time are: C<weekday, month,>
+C<monthnum, day, year, hour24, hour, ampm, minute,> and C<second.>
+
+=back
+
+=head1 CONFIGURATION
+
+The following fields in the configuration file are used:
+
+=over 4
+
+=item gallery_file
+
+The name of the file containing the photo gallery. By default, this name
+is index.html.
+
+=item gallery_include
+
+A wildcard expression indicating the files that are photos to be included in the
+gallery. The default is '*.jpg',
+
+=item gallery_template
+
+The name of the template used to produce the photo gallery. The default is
+'gallery.htm'.
+
+=item thumb_suffix
+
+The suffix added to the photo name to produce the thumb photo name. The default
+is '-thumb'.
+
+=item thumb_width
+
+The width of the thumb photos. Leave at 0 if the width is defined to be
+proportional to the height.
+
+=item thumb_height
+
+The height of the thumb photos. Leave at 0 if the height is defined to be
+proportional to the width.
+
+=item photo_width
+
+The width of the photo after resizing. Leave at 0 if the width is defined to be
+proportional to the height.
+
+=item photo_height
+
+The height of the photo after resizing. Leave at 0 if the height is defined to
+be proportional to the width.
+
+=head1 LICENSE
+
+Copyright (C) Bernie Simon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Bernie Simon E<lt>bernie.simon@gmail.comE<gt>
+
+=cut
---
+++ t/CreateGallery.t
@@ -0,0 +1,141 @@
+#!/usr/bin/env perl
+use strict;
+
+use IO::File;
+use File::Path qw(rmtree);
+use File::Spec::Functions qw(catdir catfile rel2abs splitdir);
+
+use Test::More tests => 17;
+
+#----------------------------------------------------------------------
+# Load package
+
+my @path = splitdir(rel2abs($0));
+pop(@path);
+pop(@path);
+
+my $lib = catdir(@path, 'lib');
+unshift(@INC, $lib);
+
+require App::Followme::CreateGallery;
+
+my $test_dir = catdir(@path, 'test');
+my $data_dir = catdir(@path, 'tdata');
+
+rmtree($test_dir);
+mkdir $test_dir;
+chdir $test_dir;
+
+my $template_name = 'gallery_template.htm';
+
+my $configuration = {
+                    gallery_file => 'index.html',
+                    gallery_include => '*.jpg',
+                    gallery_template => $template_name,
+                    thumb_suffix => '-thumb',
+                    web_extension => 'html',
+                    photo_height => 600,
+                    thumb_height => 150,
+                    };
+
+#----------------------------------------------------------------------
+# Test support routines
+
+do {
+    my $gal = App::Followme::CreateGallery->new($configuration);
+    my $filename = catfile($test_dir, 'myphoto.jpg');
+    my $thumbname = $gal->build_thumb_name($filename);
+    is($thumbname, catfile($test_dir, 'myphoto-thumb.jpg'),
+       'build thumb name'); # test 1
+
+    my $exclude = $gal->get_excluded_files();
+    is($exclude, '*-thumb.jpg', 'excluded files'); # test 2
+
+    my %configuration = %$configuration;
+    $configuration{photo_height} = 600;
+    $configuration{thumb_height} = 150;
+    $gal = App::Followme::CreateGallery->new(\%configuration);
+    my ($width, $height) = $gal->new_size('photo', 1800, 1200);
+    is($width, 900, 'photo width'); # test 3
+    is($height, 600, 'photo height'); # test 4
+
+    %configuration = %$configuration;
+    $configuration{photo_width} = 600;
+    $configuration{thumb_width} = 150;
+    $gal = App::Followme::CreateGallery->new(\%configuration);
+    ($width, $height) = $gal->new_size('thumb', 1800, 1200);
+    is($width, 150, 'thumb width'); # test 5
+    is($height, 100, 'thumb height'); # test 6
+};
+
+#----------------------------------------------------------------------
+# Create gallery
+
+do {
+   my $gallery_template = <<'EOQ';
+<html>
+<head>
+<meta name="robots" content="noarchive,follow">
+<!-- section meta -->
+<title>$title</title>
+<!-- endsection meta -->
+</head>
+<body>
+<!-- section content -->
+<h1>$title</h1>
+
+<ul>
+<!-- for @loop -->
+<li><img src="$thumb_url" width="$thumb_width" height="$thumb_height" /><br />
+<a href="$photo_url">$title</a></li>
+<!-- endfor -->
+</ul>
+<!-- endsection content -->
+</body>
+</html>
+EOQ
+
+    my $gal = App::Followme::CreateGallery->new($configuration);
+    $gal->write_page($template_name, $gallery_template);
+
+    my $gallery_dir = catfile($test_dir, 'gallery');
+    mkdir($gallery_dir);
+    chdir($gallery_dir);
+
+    my @photo_files;
+    my @thumb_files;
+    foreach my $count (qw(first second third)) {
+        my $filename = '*-photo.jpg';
+        $filename =~ s/\*/$count/g;
+
+        my $input_file = catfile($data_dir, $filename);
+        my $output_file = catfile($gallery_dir, $filename);
+
+        my $photo = $gal->read_photo($input_file);
+        $gal->write_photo($output_file, $photo);
+        ok(-e $output_file, "read and write photo $count"); # test 7-9
+
+        push(@photo_files, $output_file);
+        my $thumb_file = $gal->build_thumb_name($output_file);
+        push(@thumb_files, $thumb_file);
+    }
+
+    my $data = $gal->gallery_data($gallery_dir, \@photo_files);
+    is($data->[0]{title}, 'First Photo', 'First page title'); # test 10
+    is($data->[1]{photo_url}, 'second-photo.jpg', 'Second page url'); # test 11
+    is($data->[2]{thumb_url}, 'third-photo-thumb.jpg',
+       'Third page thumb url'); # test 12
+
+    foreach my $i (1 .. 3) {
+        ok(-e $thumb_files[$i-1], "Create thumb $i"); # test 13-15
+    }
+
+    my $gallery_name = $gal->full_file_name($gallery_dir, $gal->{gallery_file});
+    $gal->create_a_gallery($gallery_dir, $gallery_name);
+
+    ok(-e $gallery_name, 'Create index file'); # test 16
+
+    my $page = $gal->read_page($gallery_name);
+    my @items = $page =~ m/(<li>)/g;
+    is(@items, 3, 'Index three photos'); # test 17
+};
