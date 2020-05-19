1f54ba25d6c048e74ef12336b688b2f0f5558d67
Bernie Simon <bernie.simon@gmail.com>
2020-05-19 11:46:49 -0400

    Check for methods of Pod::Simple::XHTML before calling them

--- lib/App/Followme/PodData.pm
+++ lib/App/Followme/PodData.pm
@@ -141,8 +141,11 @@ sub initialize_parser {
 
     my $psx = Pod::Simple::XHTML->new();
 
-    $psx->html_h_level($h_level);
-    $psx->perldoc_url_prefix($self->{site_url});
+    $psx->html_h_level($h_level) 
+		if $psx->can('html_h_level');
+
+    $psx->perldoc_url_prefix($self->{site_url}) 
+		if $psx->can('perldoc_url_prefix');
 
     return $psx;
 }
