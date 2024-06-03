25c739a3ba43e4c9854e0845b19f0d56d123a3b5
Bernie Simon <bernie.simon@gmail.com>
2024-06-03 10:30:57 -0400

    Updated CreateIndex.pm to correctly handle deleted files.

--- lib/App/Followme/CreateIndex.pm
+++ lib/App/Followme/CreateIndex.pm
@@ -95,10 +95,8 @@ sub update_folder {
     my $base_index_file = $self->to_file($self->{data}{base_directory}); 
 
     my $template_file = $self->get_template_name($self->{template_file});
-    my $newest_file = $self->{data}->build('newest_file', $base_index_file);
 
-
-    unless (fio_is_newer($index_file, $template_file, @$newest_file) &&
+    unless (fio_is_newer($index_file, $template_file, $folder) &&
             $self->sections_are_filled($index_file)) {
 
         my $page = $self->render_file($self->{template_file}, $base_index_file);
